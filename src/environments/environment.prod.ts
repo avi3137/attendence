471df7be7b18e646ff646246ddb870bd4e2f3ebe
avi3137 <m.chikhale3137@gmail.com>
2020-01-02 00:22:36 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDOMgmEGcp4h4a9QtWwZ9vwKgHXcycN4ug",
+    authDomain: "zealonic-7ac.firebaseapp.com",
+    databaseURL: "https://zealonic-7ac.firebaseio.com",
+    projectId: "zealonic-7ac",
+    storageBucket: "zealonic-7ac.appspot.com",
+    messagingSenderId: "589785739980",
+    appId: "1:589785739980:web:a0a22f5fbdafb900c3c869",
+    measurementId: "G-SNNHRPWV6Q"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  googleMapsKey: 'AIzaSyBQQ85Q0MJCGDnjqs5KTriDOeq3cz54MZE',
+  firebase: {
+    apiKey: "AIzaSyDOMgmEGcp4h4a9QtWwZ9vwKgHXcycN4ug",
+    authDomain: "zealonic-7ac.firebaseapp.com",
+    databaseURL: "https://zealonic-7ac.firebaseio.com",
+    projectId: "zealonic-7ac",
+    storageBucket: "zealonic-7ac.appspot.com",
+    messagingSenderId: "589785739980",
+    appId: "1:589785739980:web:a0a22f5fbdafb900c3c869",
+    measurementId: "G-SNNHRPWV6Q"
+  },
+  emailAPI: 'xxxx',
+  database: 'firebase',
+  social: {
+    fblink: 'https://www.facebook.com/elishconsulting',
+    linkedin: 'https://www.linkedin.com/in/ashuklax/',
+    github: 'https://github.com/AmitXShukla',
+    emailId: 'mailto://amit@elishconsulting.com'
+  },
+  socialAuthEnabled: true,
+  
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
