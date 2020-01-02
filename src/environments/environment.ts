029875e0bb3d0c049ce54063371740fd2b4cd31c
Avinash Chikhale <43858815+avi3137@users.noreply.github.com>
2020-01-02 13:06:47 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,24 +4,24 @@
 
 export const environment = {
   production: true,
-  googleMapsKey: 'AIzaSyBQQ85Q0MJCGDnjqs5KTriDOeq3cz54MZE',
+  googleMapsKey: 'xxxx',
   firebase: {
-    apiKey: "AIzaSyDOMgmEGcp4h4a9QtWwZ9vwKgHXcycN4ug",
-    authDomain: "zealonic-7ac.firebaseapp.com",
-    databaseURL: "https://zealonic-7ac.firebaseio.com",
+    apiKey: "xxxx",
+    authDomain: "Xxxx",
+    databaseURL: "xxx",
     projectId: "zealonic-7ac",
-    storageBucket: "zealonic-7ac.appspot.com",
-    messagingSenderId: "589785739980",
-    appId: "1:589785739980:web:a0a22f5fbdafb900c3c869",
-    measurementId: "G-SNNHRPWV6Q"
+    storageBucket: "zzzzm",
+    messagingSenderId: "zzzz",
+    appId: "xxx",
+    measurementId: "G-xxx"
   },
   emailAPI: 'xxxx',
   database: 'firebase',
   social: {
-    fblink: 'https://www.facebook.com/elishconsulting',
-    linkedin: 'https://www.linkedin.com/in/ashuklax/',
-    github: 'https://github.com/AmitXShukla',
-    emailId: 'mailto://amit@elishconsulting.com'
+    fblink: 'xxx',
+    linkedin: 'xxx',
+    github: 'xxx',
+    emailId: 'xxxx'
   },
   socialAuthEnabled: true,
   
